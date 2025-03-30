a384c1b2355fcd467192742b1e090d867cbfc4ee
TristanNguyen04 <nquocdung.04@gmail.com>
2025-03-31 00:38:26 +0800

    Reset Project

--- app/src/main/java/com/example/smarthealth/MainActivity.java
+++ app/src/main/java/com/example/smarthealth/MainActivity.java
@@ -14,39 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceBundle) {
         super.onCreate(savedInstanceBundle);
-
-        // Check if the user is logged in
-        SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
-        boolean isLoggedIn = preferences.getBoolean("isLoggedIn", false);
-
-        if (!isLoggedIn) {
-            // Redirect to LoginActivity
-            Intent intent = new Intent(this,LoginActivity.class);
-            startActivity(intent);
-            finish();
-            return; // Stop further execution of onCreate
-        }
-
         setContentView(R.layout.activity_main);
 
-        Button logoutButton = findViewById(R.id.navbar_medbot_btn);
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                logout();
-            }
-        });
-    }
-
-    public void logout() {
-        SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("isLoggedIn", false);
-        editor.apply();
-
-        // Redirect to LoginActivity
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
     }
 }
