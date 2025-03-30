49c8d91acf260671dac630fbf3658119afeb355d
TristanNguyen04 <nquocdung.04@gmail.com>
2025-03-30 22:52:22 +0800

    Done User Authentication (Will do Facebook if have time)

---
+++ app/src/main/java/com/example/smarthealth/AuthResponse.java
@@ -0,0 +1,10 @@
+package com.example.smarthealth;
+
+public class AuthResponse {
+    private String status;
+    private String message;
+
+    public String getStatus() { return status; }
+    public String getMessage() { return message; }
+}
+
--- app/src/main/java/com/example/smarthealth/MainActivity.java
+++ app/src/main/java/com/example/smarthealth/MainActivity.java
@@ -29,36 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-//        View mainContentView = findViewById(R.id.main_content_view);
-//        mainContentView.setOnTouchListener(new View.OnTouchListener() {
-//            private float dY;
-//            private float originalY = -1;
-//
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                switch (event.getAction()) {
-//                    case MotionEvent.ACTION_DOWN:
-//                        if (originalY == -1) {
-//                            mainContentView.post(new Runnable() {
-//                                @Override
-//                                public void run() {
-//                                    originalY = mainContentView.getY();
-//                                }
-//                            });
-//                        }
-//                        dY = v.getY() - event.getRawY();
-//                        return true;
-//                    case MotionEvent.ACTION_MOVE:
-//                        float newY = event.getRawY() + dY;
-//                        if (newY >= originalY) {
-//                            v.setY(newY);
-//                        }
-//                        return true;
-//                }
-//                return false;
-//            }
-//        });
-
         Button logoutButton = findViewById(R.id.navbar_medbot_btn);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
