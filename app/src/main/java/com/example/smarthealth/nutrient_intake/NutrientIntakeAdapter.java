2acde1e556ddf775c4fca7ad968a59ad1af45397
itsmeox <ongxuan1028@gmail.com>
2025-04-02 19:45:11 +0800

    Show red when excessive nutrient is taken

--- app/src/main/java/com/example/smarthealth/nutrient_intake/NutrientIntakeAdapter.java
+++ app/src/main/java/com/example/smarthealth/nutrient_intake/NutrientIntakeAdapter.java
@@ -1,5 +1,6 @@
 package com.example.smarthealth.nutrient_intake;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,8 +32,16 @@ public class NutrientIntakeAdapter extends RecyclerView.Adapter<NutrientIntakeVi
     @Override
     public void onBindViewHolder(@NonNull NutrientIntakeViewHolder holder, int position) {
         NutrientIntake currNutrientIntake = nutrientIntakeList.get(position);
-        holder.totalNutrientText.setText(String.valueOf(currNutrientIntake.getTotalNutrient()) + currNutrientIntake.getIntakeUnit());
-        holder.currentNutrientText.setText(String.valueOf(currNutrientIntake.getCurrentNutrient()) + currNutrientIntake.getIntakeUnit());
+        double currentNutrient = currNutrientIntake.getCurrentNutrient();
+        double totalNutrient = currNutrientIntake.getTotalNutrient();
+
+        holder.currentNutrientText.setText(String.valueOf(currentNutrient) + currNutrientIntake.getIntakeUnit());
+        holder.totalNutrientText.setText(String.valueOf(totalNutrient) + currNutrientIntake.getIntakeUnit());
+
+        if (currentNutrient > totalNutrient) {
+            holder.currentNutrientText.setTextColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.warning_red));
+        }
+
         holder.nutrientTitle.setText(currNutrientIntake.getNutrientName());
         holder.nutrientIcon.setImageDrawable(
                 ContextCompat.getDrawable(holder.itemView.getContext(), currNutrientIntake.getNutrientIconId()));
