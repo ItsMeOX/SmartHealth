19b65672fd074a1eafb4d443fb00b8efddbf916a
itsmeox <ongxuan1028@gmail.com>
2025-04-02 18:02:03 +0800

    Add bot suggestion provider

---
+++ app/src/main/java/com/example/smarthealth/bot_suggestions/DatabaseBotSuggestionProvider.java
@@ -0,0 +1,33 @@
+package com.example.smarthealth.bot_suggestions;
+
+import com.example.smarthealth.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseBotSuggestionProvider implements BotSuggestionProvider {
+    @Override
+    public List<BotSuggestion> getBotSuggestions() {
+        String[] titles = {"Increase Protein Intake", "Choose complex carbohydrates", "Stay hydrated", "test", "test2"};
+        String[] descriptions = {"Increase in take of fish, chicken, eggs, tofu, legumes to...",
+                "Choose complex carbohydrates for example whole grains, vegetables to avoid sugar spikes. Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao." +
+                        "Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao."
+                        +"Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao."
+                        +"Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao."
+                        +"Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao."
+                        +"Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao."
+                        +"Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao."
+                        +"Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.Lorem ipsum dolmao.",
+                "Drink more water, or herbal teas, soups to stay hydrated, because H2O make you human.",
+                "test",
+                "test2"};
+
+        List<BotSuggestion> botSuggestions = new ArrayList<>();
+        for (int i = 0; i < titles.length; i++) {
+            botSuggestions.add(new BotSuggestion(titles[i], descriptions[i], R.drawable.bot_suggestion_image));
+        }
+
+        return botSuggestions;
+    }
+
+}
