2c5dc52c3e1da2e2469042a7716502ce321dc108
Akirahai <lehaivin03@gmail.com>
2025-04-03 22:28:23 +0800

    fix

--- app/src/main/java/com/example/smarthealth/chatbot/ChatBot.java
+++ app/src/main/java/com/example/smarthealth/chatbot/ChatBot.java
@@ -1,12 +1,5 @@
 package com.example.smarthealth.chatbot;
 
-import android.os.Handler;
-import android.os.Looper;
-
-
-import androidx.annotation.NonNull;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,13 +7,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class ChatBot {
     private static final String API_URL = "https://api.openai.com/v1/chat/completions";
